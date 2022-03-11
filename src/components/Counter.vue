d5d72abac2642cfeafcee2fd7b738ff10c79aa6b
Papernood <395404607@qq.com>
2022-03-11 20:17:13 +0800

    改进了计数按钮

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <van-stepper slot="right-part" v-model="value" min="1"/>
+</template>
+<script>
+export default {
+  name: 'CounterComponent',
+  data () {
+    return {
+      value: 1
+    }
+  }
+}
+</script>
