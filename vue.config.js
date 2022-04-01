51f813fd7041d2ac0a509a0c86b0cbb1a3417093
Papernood <395404607@qq.com>
2022-04-01 21:27:42 +0800

    路径

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: true,
+  publicPath: './',
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    //  proxy: {
+    //   '/': {
+    //     target: 'localhost:3000/', // 要跨域的域名
+    //     // changeOrigin: true // 是否开启跨域
+    //   }
+    // }
+  }
+    
 })
